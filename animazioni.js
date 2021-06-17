02fa2381795b482609cc2f9da04318f78f9dbc8a
Emanuele <emanuele.bacciu@gmail.com>
2021-06-17 09:53:13 +0200

    lavoro terminato

---
+++ animazioni.js
@@ -0,0 +1,174 @@
+ScrollReveal().reveal('.reveal',{ distance: '100px', duration: 1500, easing: 'cubic-bezier(0.5, 0, 0, 1)', interval: 600, mobile: false});
+ScrollReveal().reveal('.reveal__lateral__right',{ distance: '500px', origin:'right', duration: 1500, easing: 'cubic-bezier(0.5, 0, 0, 1)', interval: 600, mobile: false});
+ScrollReveal().reveal('.reveal__lateral__left',{ distance: '500px', origin:'left', duration: 1500, easing: 'cubic-bezier(0.5, 0, 0, 1)', interval: 600, mobile: false});
+
+//aminiamo 
+$(document).ready(function(){
+    $(".portfolio__img img ").click(function(){
+        $(".portfolio__zoom1").addClass("visibile");
+        $(".portfolio").addClass("invisibile");
+      });
+      $("span").click(function(){
+        $(".portfolio__zoom1").removeClass("visibile");
+        $(".portfolio").removeClass("invisibile");
+      });
+
+//seconda
+      $(".portfolio__img2 img ").click(function(){
+        $(".portfolio__zoom__2").addClass("visibile");
+        $(".portfolio").addClass("invisibile");
+      });
+      $("span").click(function(){
+        $(".portfolio__zoom__2").removeClass("visibile");
+        $(".portfolio").removeClass("invisibile");
+      });
+
+//TERZA
+$(".portfolio__img3 img ").click(function(){
+    $(".portfolio__zoom__3").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__3").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+
+//QUARTA
+$(".portfolio__img4 img ").click(function(){
+    $(".portfolio__zoom__4").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__4").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//QUINTA
+$(".portfolio__img5 img ").click(function(){
+    $(".portfolio__zoom__5").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__5").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//SESTA
+$(".portfolio__img6 img ").click(function(){
+    $(".portfolio__zoom__6").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__6").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//SETTIMA
+$(".portfolio__img7 img ").click(function(){
+    $(".portfolio__zoom__7").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__7").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//OTTAVA
+$(".portfolio__img8 img ").click(function(){
+    $(".portfolio__zoom__8").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__8").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//NONA
+$(".portfolio__img9 img ").click(function(){
+    $(".portfolio__zoom__9").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__9").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//DECIMA
+$(".portfolio__img10 img ").click(function(){
+    $(".portfolio__zoom__10").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__10").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//UNDICESIMA
+$(".portfolio__img11 img ").click(function(){
+    $(".portfolio__zoom__11").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__11").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//DODICESIMA
+$(".portfolio__img12 img ").click(function(){
+    $(".portfolio__zoom__12").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__12").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//TREDICESIMA
+$(".portfolio__img13 img ").click(function(){
+    $(".portfolio__zoom__13").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__13").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//QUATTORDICESIMA
+$(".portfolio__img14 img ").click(function(){
+    $(".portfolio__zoom__14").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__14").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//QUINDICESIMA
+$(".portfolio__img15 img ").click(function(){
+    $(".portfolio__zoom__15").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__15").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//SEDICESIMA
+$(".portfolio__img16 img ").click(function(){
+    $(".portfolio__zoom__16").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__16").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+//DICIASSETTESIMA
+$(".portfolio__img17 img ").click(function(){
+    $(".portfolio__zoom__17").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__17").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+
+//DICIOTTESIMA
+$(".portfolio__img18 img ").click(function(){
+    $(".portfolio__zoom__18").addClass("visibile");
+    $(".portfolio").addClass("invisibile");
+  });
+  $("span").click(function(){
+    $(".portfolio__zoom__18").removeClass("visibile");
+    $(".portfolio").removeClass("invisibile");
+  });
+
+  });
+
